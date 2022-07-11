7bcd2820c75124c024a7e3c172525e0fd59aea51
Marco Valerio <mo.valerio93@gmal.com>
2022-07-11 16:51:33 +0200

    lista spesa done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-const lista_spesa = ['banane', 'fragole', 'mirtilli', 'melanzane', 'carote', 'latte', 'pasta','biscotti',]
+/*
+1 creiamo array
+2 scorri arrey 
+3 stampa arrey
+
+*/
+const lista_spesa = ['banane', 'fragole', 'mirtilli', 'melanzane', 'carote', 'latte', 'pasta','biscotti',];
+let start;
+let i = 0;
+
+while (i < lista_spesa.length){
+    alert(lista_spesa[i])
+    i++;
+}
+
+
+
+
+
 
